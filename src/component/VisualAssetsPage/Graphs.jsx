f96c452fb5b63fd0113a4d3b95b5235e1bc6bb0d
Hamin Lee <whereanna7@gmail.com>
2024-08-19 22:37:54 +0100

    [ADD] apexchart library install

--- src/component/VisualAssetsPage/Graphs.jsx
+++ src/component/VisualAssetsPage/Graphs.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import Chart from "react-apexcharts";
+import styled from "styled-components";
+
+const GraphsContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 30px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  @media (min-width: 600px) {
+    max-width: 750px;
+    flex-direction: row;
+  }
+
+  @media (min-width: 800px) {
+    max-width: 900px;
+  }
+
+  @media (min-width: 1150px) {
+    max-width: 1000px;
+  }
+
+  @media (min-width: 1280px) {
+    max-width: 1248px;
+  }
+`;
+
+const InnerContainer = styled.div`
+  width: 100%;
+`;
 
 function Graphs() {
-  return <div>Graphs</div>;
+  const chartOptions = {
+    chart: {
+      height: 300,
+      type: "line",
+      toolbar: { show: false },
+      background: "transparent",
+    },
+    stroke: { curve: "smooth", width: 4 },
+    grid: { show: false },
+    yaxis: { show: false },
+    xaxis: {
+      labels: { show: false },
+      axisTicks: { show: false },
+      axisBorder: { show: false },
+      categories: ["2022-01-01", "2022-01-02", "2022-01-03"],
+      type: "datetime",
+    },
+    colors: ["#FF1654", "#247BA0"],
+    tooltip: {
+      y: { formatter: (value) => value.toFixed(2) },
+    },
+  };
+
+  const chartSeries = [
+    { name: "Price 1", data: [1000, 2000, 3000] },
+    { name: "Price 2", data: [1500, 2500, 3500] },
+  ];
+
+  return (
+    <GraphsContainer>
+      <InnerContainer>
+        <p>1. Line Chart</p>
+        <Chart
+          options={chartOptions}
+          series={chartSeries}
+          type="line"
+          height={300}
+        />
+      </InnerContainer>
+    </GraphsContainer>
+  );
 }
 
 export default Graphs;
